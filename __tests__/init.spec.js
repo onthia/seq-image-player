596591859cd80e73d0e715a921c2e2840089f91a
liuzhichun <liuzhichun@vv.cn>
2021-04-21 16:37:56 +0800

    0.1.1

--- __tests__/init.spec.js
+++ __tests__/init.spec.js
@@ -1,12 +1,15 @@
-const { SeqImagePlayer } = require('../src/index.ts');
+import { SeqImagePlayer } from '../src/index.ts';
 
 test('initialize player instance', () => {
+  const container = document.createElement('div');
   const option = {
     pathTemplate: 'https://workvideo.vvtechnology.cn/%E5%BA%8F%E5%88%97%E5%9B%BE750-%20600_20s_19Mar{*}.jpg',
     imageNumber: 10,
     wildcardLength: 4,
     container: document.createElement('div'),
   };
-  const player = new SeqImagePlayer(option);
-  expect(player.name).toBe('seqImagePlayer');
+  new SeqImagePlayer(option);
+  setTimeout(() => {
+    expect(container.firstChild.tagName).toBe('CANVAS');
+  }, 1000);
 });
